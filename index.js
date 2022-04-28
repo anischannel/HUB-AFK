bc64b702d3b106fd03197bffabf3535c035c03d7
anis <73384400+anischannel@users.noreply.github.com>
2022-04-28 14:37:25 +0530

    HUB AFK

---
+++ index.js
@@ -0,0 +1,109 @@
+const mineflayer = require('mineflayer')
+const cmd = require('mineflayer-cmd').plugin
+const fs = require('fs');
+let rawdata = fs.readFileSync('config.json');
+let data = JSON.parse(rawdata);
+var lasttime = -1;
+var moving = 0;
+var connected = 0;
+var actions = [ 'jump']
+var lastaction;
+var pi = 3.14159;
+var moveinterval = 0; // 2 second movement interval
+var maxrandom = 1; // 0-5 seconds added to movement interval (randomly)
+var host = data["ip"];
+var username = data["name"]
+var nightskip = data["auto-night-skip"]
+var bot = mineflayer.createBot({
+  host: host,
+  username: username,
+});
+function getRandomArbitrary(min, max) {
+       return Math.random() * (max - min) + min;
+
+}
+
+bot.loadPlugin(cmd)
+
+bot.on('login',function(){
+	console.log("going in for login")
+	bot.chat('/login 06060606');
+});
+bot.on('login',function(){
+	console.log("going in for login")
+	bot.chat("hello GUYS I AM HERE TO AFK");
+});
+
+
+
+
+// bot.on('login',function(){
+// 	console.log("Logged In")
+// 	bot.chat('/premium TPSMAINTAINER');
+// });
+
+bot.on('time', function(time) {
+	if(nightskip == "true"){
+	if(bot.time.timeOfDay >= 13000){
+	bot.chat('/time set day')
+	}}
+    if (connected <1) {
+        return;
+    }
+    if (lasttime<0) {
+        lasttime = bot.time.age;
+    } else {
+        var randomadd = Math.random() * maxrandom * 20;
+        var interval = moveinterval*20 + randomadd;
+        if (bot.time.age - lasttime > interval) {
+            if (moving == 1) {
+                bot.setControlState(lastaction,false);
+                moving = 0;
+                lasttime = bot.time.age;
+            } else {
+                var yaw = Math.random()*pi - (0.5*pi);
+                var pitch = Math.random()*pi - (0.5*pi);
+                bot.look(yaw,pitch,false);
+                lastaction = actions[Math.floor(Math.random() * actions.length)];
+                bot.setControlState(lastaction,true);
+                moving = 1;
+                lasttime = bot.time.age;
+                bot.activateItem();
+            }
+        }
+    }
+});
+
+bot.on('spawn',function() {
+    connected=1;
+});
+
+bot.on('death',function() {
+    bot.emit("respawn")
+});
+
+bot.on('kicked', (reason) =>
+      console.log(
+         '\x1b[33m',
+         `[BotLog] Bot was kicked from the server. Reason: \n${reason}`,
+         '\x1b[0m'
+      )
+   );
+
+
+
+require('http').createServer((req, res) => res.end(`
+ |-----------------------------------------|
+ |              Informations               |
+ |-----------------------------------------|
+ |• Alive: 24/7                            |
+ |-----------------------------------------|
+ |• Author: Supreme#2401                   |
+ |-----------------------------------------|
+ |• Server: https://discord.gg/gU7XAxTpX5  |
+ |-----------------------------------------|
+ |• Github: https://github.com/diwasatreya |
+ |-----------------------------------------|
+ |• License: Apache License 2.0            |
+ |-----------------------------------------|
+`)).listen(3000)
